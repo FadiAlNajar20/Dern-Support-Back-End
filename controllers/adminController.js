398d1c2e6da2a9acd73d77adde97b51727748a6f
put your github username here <muathmhawich1@gmail.com>
2024-08-23 16:35:27 +0300

    modify response/admin/login/controler/ add new route in admin / viewAllReport

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -45,7 +45,11 @@ export const login = async (req, res) => {
     const token = jwt.sign({ id: admin.id }, process.env.JWT_SECRET, {
       expiresIn: "24h",
     });
-    res.json({ token });
+    res.status(200).json({
+      message: "Login successfully",
+      success: true,
+      token,
+    });
   } catch (err) {
     console.error("Login error:", err);
     res.status(500).json({ error: "Failed to login" });
@@ -637,5 +641,44 @@ export const getReportForRequest = async (req, res) => {
   } catch (err) {
     console.error("Error fetching reports:", err);
     res.status(500).json({ message: "Failed to fetch report data" });
+  }
+};
+//=============================/admin/reports/details========================================
+export const getAllReportDetails = async (req, res) => {
+  try {
+    const query = `
+      SELECT 
+        r.id AS reportid,
+        r.comment AS reportcomment,
+        req.customerid,
+        req.technicianid,
+        u_t.name AS technicianname,
+        u_t.email AS technicianemail,
+        u_c.name AS customername,
+        u_c.email AS customeremail,
+        sp.name AS spareName,
+        rd.quantity AS spareQuantity,
+        sp.price AS sparePrice
+      FROM report r
+      JOIN request req ON r.requestid = req.id
+      JOIN technician t ON req.technicianid = t.id
+      JOIN customer c ON req.customerid = c.id
+      JOIN "User" u_t ON t.userid = u_t.id
+      JOIN "User" u_c ON c.userid = u_c.id
+      LEFT JOIN reportdetails rd ON r.id = rd.reportid
+      LEFT JOIN spares sp ON rd.spareid = sp.id
+      ORDER BY r.id;
+    `;
+    
+    const { rows } = await client.query(query);
+console.log("ok");
+
+    res.json({
+      message: 'All report data retrieved successfully',
+      reportDetails: rows
+    });
+  } catch (error) {
+    console.error('Error fetching reports:', error);
+    res.status(500).json({ message: 'Failed to fetch reports' });
   }
 };
